21079bf33442f72adeb8f864665e4a9d2e3212cb
leomiranda1995 <leomiranda1995@gmail.com>
2021-04-17 03:03:27 -0300

    Enum

---
+++ 13 - Enumerações/Course/Course/Entities/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Course.Entities.Enums;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public Order(int id, DateTime moment, OrderStatus status)
+        {
+            Id = id;
+            Moment = moment;
+            Status = status;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Moment}, {Status}";
+        }
+    }
+}
---
+++ 13 - Enumerações/Course/Course/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Course.Entities;
+using Course.Entities.Enums;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Order order = new Order(1080,DateTime.Now, OrderStatus.Processing);
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+    }
+}
