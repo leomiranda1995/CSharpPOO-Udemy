7303cd9574eb2e9e8e1e9ec9ab2ddab2d056e949
leomiranda1995 <leomiranda1995@gmail.com>
2021-04-17 03:05:25 -0300

    Solução base zerada.

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -8,29 +8,7 @@ namespace Course
     {
         private static void Main(string[] args)
         {
-            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
-
-            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
-
-            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
-
-            Console.WriteLine("d1");
-            Console.WriteLine($"{d1}\n{d1.Kind}\n{d1.ToLocalTime()}\n{d1.ToUniversalTime()}");
-            Console.WriteLine();
-
-            Console.WriteLine("d2");
-            Console.WriteLine($"{d2}\n{d2.Kind}\n{d2.ToLocalTime()}\n{d2.ToUniversalTime()}");
-            Console.WriteLine();
-
-            Console.WriteLine("d3");
-            Console.WriteLine($"{d3}\n{d3.Kind}\n{d3.ToLocalTime()}\n{d3.ToUniversalTime()}");
-            Console.WriteLine();
-
-            DateTime d4 = DateTime.Parse("2000-08-15 13:05:58");
-            Console.WriteLine(d4);
-
-            DateTime d5 = DateTime.Parse("2000-08-15T13:05:58Z");
-            Console.WriteLine(d5);
+            
         }
     }
 }
